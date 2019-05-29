496de0683d01e9ad48b43bfadb5185b91c05f0e1
aakashDeveloper <ahanda205@gmail.com>
2019-05-29 03:24:59 +0100

    a

---
+++ javascript/five.js
@@ -0,0 +1,34 @@
+var a = 10
+if( a == 10){
+    console.log("a is acceptable")
+} else if(a == 11) {
+    console.log("a is ok")
+}
+else {
+    console.log("a is rejected")
+}
+
+var a = 10
+if( a == 10){
+    console.log("a is acceptable")
+} 
+if(a == 11) {
+    console.log("a is ok")
+}
+
+
+////////////////////////
+var input = 1 
+var input = prompt('Please eneter value')
+if (input%2 == 0) {
+    console.log('number is even')
+} else {
+    console.log('number is odd')
+}
+
+var confirm1 = confirm('do you want to leave')
+if (confirm1 == true) {
+    window.location.href="http://aakashhanda.me"
+} else{
+    console.log( 'ok we are not going')
+}
