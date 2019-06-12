85b94993c16d84806055ebb4f6df6c9d4ecf29c8
aakashDeveloper <ahanda205@gmail.com>
2019-06-12 03:15:33 +0100

    a

---
+++ javascript/fifteenmouseover.js
@@ -0,0 +1,21 @@
+function mouseO(mydiv){
+    mydiv.style.backgroundColor = "red";
+    mydiv.innerHTML = "Release Me"
+}
+
+function mouseOut(mydiv){
+    mydiv.style.backgroundColor = "green";
+    mydiv.innerHTML = "Over Me"
+}
+
+
+function mouseD(mydiv){
+    mydiv.style.backgroundColor = "teal";
+    mydiv.innerHTML = "Release Me"
+}
+
+function mouseU(mydiv){
+    mydiv.style.backgroundColor = "tomato";
+    mydiv.innerHTML = "Click Me"
+}
+
